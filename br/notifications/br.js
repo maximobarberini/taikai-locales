21adf218f001d63177516ce88cfcf04728cf2cb5
Henrique Macedo <hey@ickas.dev>
2023-10-23 16:49:01 +0100

    chore: add notifications

---
+++ br/notifications/br.js
@@ -0,0 +1,69 @@
+/* eslint-disable max-len */
+module.exports = {
+  allocateReceived:
+    'Alocação de {{amount}} VKAI no {{{challengeName}}} para {{{to}}} está confirmada',
+  transferReceived: 'Transferência global de {{amount}} TKAI de {{{from}}} recebida com sucesso',
+  transferSent: 'Transferência global de {{amount}} TKAI para {{{to}}} confirmada com sucesso',
+  transferSentFailed: 'Transferência global de {{amount}} TKAI para {{{to}}} falhou',
+  challengeTransferReceived:
+    'Transferência de {{amount}} VKAI no desafio {{{challengeName}}} de {{{from}}} recebida com sucesso',
+  challengeTransferSent:
+    'Transferência de {{amount}} VKAI no desafio {{{challengeName}}} para {{{to}}} está confirmada',
+  challengeTransferSentFailed:
+    'Transferência de {{amount}} VKAI no desafio {{{challengeName}}} para {{{to}}} falhou',
+  backSuccess:
+    'Transação de voto de {{amount}} VKAI no  desafio {{{challengeName}}} e projeto {{{projectName}}} confirmada',
+  backFailed:
+    'Transação de voto de {{amount}} VKAI no desafio {{{challengeName}}} e projeto {{{projectName}}} falhou',
+  registeredAsBacker:
+    'Você está registado como juri no desafio {{{challengeName}}} para votar na fase {{challengeStepName}}',
+  registeredAsBackerAndInnovator:
+    'Você está registado como participante e juri no desafio {{{challengeName}}} para votar na fase {{challengeStepName}}',
+  registeredAsCrowdBacker: 'Você está registado como juri no desafio {{{challengeName}}}',
+  addedAsProjectMember:
+    'Você foi adicionado como membro do projeto {{{projectName}}} no desafio {{{challengeName}}}',
+  registeredAsInnovator: 'Você esta registado como participante no desafio {{{challengeName}}}',
+  pendingInnovatorRegistration:
+    'O seu registo de participante no desafio {{{challengeName}}} está pendente de aprovação',
+  acceptedInnovatorRegistration:
+    'O seu registo de participante no desafio {{{challengeName}}} foi aceite',
+  challengeAnnouncement: 'Anúncio do desafio {{{challengeName}}} - {{{message}}}',
+  receivedDividend: 'Você recebeu {{amount}} TKAI porque {{reason}} no desafio {{{challengeName}}}',
+  general: 'Anúncio TAIKAI - {{{message}}}',
+  organization: 'Anùncio da Organização - {{{message}}}',
+  userMail: 'Nova mensagem de {{{from}}} no tópico {{{topic}}}',
+  challengeMail:
+    'Nova mensagem de {{{from}}} relativo ao desafio {{{challenge}}} - Assunto: {{{topic}}}',
+  projectMail: 'Nova mensagem de {{{from}}} no tópico {{{project}}} - Assunto: {{{topic}}}',
+  inviteToProject:
+    'Você recebeu um convite para fazer parte do projecto {{{project}}} no desafio {{{challenge}}}',
+  applyedToProject:
+    'Você submeteu com sucesso a sua candidatura à vaga {{{title}}} no projeto {{{project}}} relativo ao desafio {{{challenge}}}',
+  projectApplicationAccepted:
+    'Parabéns!! A sua candidatura à vaga {{{title}}} no projeto {{{project}}} relativo ao desafio {{{challenge}}} foi aceite.',
+  projectApplicationRejected:
+    'Não foi desta!! A  sua candidatura à vaga {{{title}}} no projeto {{{project}}} relativo ao desafio {{{challenge}}} não foi aceite',
+  organizationInReview:
+    'O conteúdo da organização {{{organization}}} vai ser revisto. Os moderadores de conteúdo da plataforma irão validar o seu conteúdo',
+  organizationPublished:
+    '{{{organization}}} foi publicado. Apartir deste momento poderá lançar os seus desafios.',
+  challengeInReview:
+    'O conteúdo do desafio {{{challenge}}} vai ser revisto. Os moderadores de conteúdo da plataforma irão validar o seu conteúd',
+  challengePublished:
+    '{{{challenge}}} foi publicado. Poderá partilhar nos seus canais de distribuição a página do desafio.',
+  challengePublishedToAllUsers:
+    'Novo desafio "{{{challengeName}}}" foi publicado pela organização {{{organizationName}}}',
+  customMessage: '{{{message}}}',
+  referredConfirmed:
+    'O utilizador {{referred}} referenciado por si completou a validação do registo',
+  referralConfirmed:
+    'Você completou o seu registo de utilizador e completou a referência de {{referrer}}',
+  registeredAsMentor: 'Você esta registado como mentor no desafio {{{challengeName}}}',
+  challengeUpdate: '{{{challengeName}}} - Nova Atualização Publicada - {{{message}}}',
+  popMinted:
+    'Anúncio do desafio {{{challengeName}}} - Parabéns! Você fez mint de {{{numberOfPOPs}}} POP(s) com sucesso, verifique sua carteira "{{{walletAddress}}}".',
+  depositReceived: 'Depósito de {{amount}} TKAI de {{{from}}} recebido com sucesso',
+  depositFailed: 'Depósito de {{amount}} TKAI de {{{from}}} falhou',
+  withdrawFailed: 'O saque de {{amount}} TKAI para a carteira {{to}} falhou',
+  withdrawSuccess: 'O saque de {{amount}} TKAI para a carteira {{to}} foi realizado com sucesso',
+};
---
+++ en/notifications/en.js
@@ -0,0 +1,63 @@
+/* eslint-disable max-len */
+module.exports = {
+  allocateReceived:
+    'Allocate of {{amount}} VKAI in {{{challengeName}}} to {{{to}}} transaction confirmed',
+  transferReceived: 'Global Transfer of {{amount}} TKAI from {{{from}}} received with success',
+  transferSent: 'Global Transfer of {{amount}} TKAI to {{{to}}} confirmed with success',
+  transferSentFailed: 'Global Transfer of {{amount}} TKAI to {{{to}}} failed',
+  challengeTransferReceived:
+    'Transfer of {{amount}} VKAI in {{{challengeName}}} from {{{from}}} received with success',
+  challengeTransferSent:
+    'Transfer of {{amount}} VKAI in {{{challengeName}}} to {{{to}}} confirmed with success',
+  challengeTransferSentFailed:
+    'Transfer of {{amount}} VKAI in {{{challengeName}}} to {{{to}}} failed',
+  backSuccess:
+    'Back of {{amount}} VKAI in {{{challengeName}}} to {{{projectName}}} transaction confirmed',
+  backFailed: 'Back of {{amount}} VKAI in {{{challengeName}}} to {{{projectName}}} failed',
+  registeredAsBacker: 'You are registered as jury on {{{challengeName}}} for {{challengeStepName}}',
+  registeredAsBackerAndInnovator:
+    'You are registered as jury and participant on {{{challengeName}}} for {{challengeStepName}}',
+  registeredAsCrowdBacker: 'You are registered as jury on {{{challengeName}}}',
+  addedAsProjectMember:
+    'You were added to {{{projectName}}} as project member for {{{challengeName}}} challenge',
+  registeredAsInnovator: 'You are registered as participant on {{{challengeName}}}',
+  pendingInnovatorRegistration: 'Your registration on {{{challengeName}}} is pending approval',
+  acceptedInnovatorRegistration: 'Your registration on {{{challengeName}}} was accepted',
+  challengeAnnouncement: 'Challenge {{{challengeName}}} announcement - {{{message}}}',
+  receivedDividend: 'Dividend of {{amount}} TKAI received for {{reason}} on {{{challengeName}}}',
+  general: 'General announcement - {{{message}}}',
+  organization: 'Organization announcement - {{{message}}}',
+  userMail: 'New mail from user {{{from}}} on topic {{{topic}}}',
+  challengeMail: 'New mail from {{{from}}} on topic {{{challenge}}} challenge - {{{topic}}}',
+  projectMail: 'New mail from {{{from}}} on topic {{{project}}} project - {{{topic}}}',
+  inviteToProject:
+    'You received an invitation to be part of {{{project}}} project on challenge {{{challenge}}}',
+  applyedToProject:
+    'You succesfully applied to {{title}} on {{{project}}} project on {{{challenge}}} challenge',
+  projectApplicationAccepted:
+    'Congratulations!! Your application to {{title}} on {{{project}}} project on {{{challenge}}} challenge was accepted.',
+  projectApplicationRejected:
+    'Sorry!! Your application to {{title}} on {{{project}}} project on {{{challenge}}} challenge was rejected.',
+  organizationInReview:
+    '{{{organization}}} content is going to be reviewed. The moderator will validate the content.',
+  organizationPublished:
+    '{{{organization}}} got published. You can now launch your own competitions.',
+  challengeInReview:
+    '{{{challenge}}} content is going to be reviewed. The moderators are going to validate the content.',
+  challengePublished:
+    '{{{challenge}}} got published. You can now share it with your partners and target participants.',
+  challengePublishedToAllUsers:
+    'New challenge "{{{challengeName}}}" was published by {{{organizationName}}}',
+  customMessage: '{{{message}}}',
+  referredConfirmed: 'The user {{referred}} referred by you confirmed your referral',
+  referralConfirmed: 'You confirmed a referral process from {{referrer}}',
+  registeredAsMentor: 'You are registered as mentor on {{{challengeName}}}',
+  challengeUpdate: 'Challenge {{{challengeName}}} Update Published - {{{message}}}',
+  popMinted:
+    'Challenge {{{challengeName}}} announcement - Congratulations! You have successfully minted {{{numberOfPOPs}}} POP(s), check your wallet "{{{walletAddress}}}".',
+  depositReceived: 'Deposit of {{amount}} TKAI from {{{from}}} received with success',
+  depositFailed: 'The deposit of {{amount}} TKAI from {{from}} has failed',
+  withdrawFailed: 'The withdraw of {{amount}} TKAI to wallet {{to}} has failed',
+  withdrawSuccess:
+    'The withdraw of {{amount}} TKAI to wallet {{to}} has been confirmed with success',
+};
